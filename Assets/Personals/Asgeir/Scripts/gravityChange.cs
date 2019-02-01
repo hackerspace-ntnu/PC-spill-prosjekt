a7293ee42c49f5b0d30e5aee4122446ecb8c776b
Krokou <asgeir@santi.no>
2019-02-01 17:31:25 +0100

    Made folders, gravityChange scripts

---
+++ Assets/Personals/Asgeir/Scripts/gravityChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityChange : MonoBehaviour {
+
+    public MovementV2 movement;
+    public Transform TF_character;
+    public Transform TF_camera;
+
+    void flipGravity()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (movement.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                movement.ourGravity *= -1;
+                TF_camera.Rotate(new Vector3(0,0,180));
+                TF_character.Rotate(new Vector3(0,0,180));
+            }
+        }
+	}
+}
