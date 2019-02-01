f7510061529bd722edc9019236c61c23c715e4e3
Krokou <asgeir@santi.no>
2019-02-01 21:24:35 +0100

    Revert "Improved changeGravity script to also rotate camera really fast"
    This reverts commit 85a4d36dea076bd8b9c00c23c38b77c9547012de.

--- Assets/Personals/Asgeir/Scripts/gravityChange.cs
+++ Assets/Personals/Asgeir/Scripts/gravityChange.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class gravityChange : MonoBehaviour {
 
@@ -9,21 +8,15 @@ public class gravityChange : MonoBehaviour {
     public Transform TF_character;
     public Transform TF_camera;
 
-    public float duration = 1f;
-
     void flipGravity()
     {
         movement.ourGravity *= -1;
-        TF_character.Rotate(new Vector3(0, 0, 180));
-
-        if (this.tag == "Player")
-        {
-            StartCoroutine(RotateCamera(duration));
-        }
+        TF_camera.Rotate(new Vector3(0,0,180));
+        TF_character.Rotate(new Vector3(0,0,180));
     }
 
 	void Start () {
-        
+		
 	}
 	
 	void Update () {
@@ -35,16 +28,4 @@ public class gravityChange : MonoBehaviour {
             }
         }
 	}
-
-    private IEnumerator RotateCamera(float duration)
-    {
-        float elapsed = 0.0f;
-        while (elapsed < duration)
-        {
-            TF_camera.transform.rotation = Quaternion.Slerp(TF_camera.transform.rotation, transform.rotation, elapsed / duration);
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-        TF_camera.transform.rotation = transform.rotation;
-    }
 }
