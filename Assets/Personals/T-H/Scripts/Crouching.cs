4e69fac1745df14da2b5a8faad0dd6dd1168a454
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2019-02-03 21:07:17 +0100

    Update Crouching.cs

--- Assets/Personals/T-H/Scripts/Crouching.cs
+++ Assets/Personals/T-H/Scripts/Crouching.cs
@@ -3,43 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Crouching : MonoBehaviour {
-
+   
+    // Crouch variables
     private BoxCollider2D hitbox;
     private MovementV2 Mv2;
-
     public bool isCrouching;
-    private Vector2 crouchedVector = new Vector2(1, 0.5f);
+    public float boxcolliderHeigth;
+    public float boxcolliderWidth;
 
-
+    // Slide variables
     private bool isSliding;
-    public  float slideSpeed = 12f;
+    public float slideSpeedModifier = 2f;
+    private float slideSpeed;
     private double slideTimer = 0f;
-    private float maxSlide = 3f;
+    public float maxSlidingTime = 3f;
  
     
 
     // Use this for initialization
     void Start () {
-        hitbox = GetComponent<BoxCollider2D>();
-        isCrouching = false;
         isSliding = false;
+        hitbox = GetComponent<BoxCollider2D>();
+        boxcolliderHeigth = hitbox.size.y;
+        boxcolliderWidth = hitbox.size.x;
         Mv2 = GetComponent<MovementV2>();
-	}
+    }
 
     // Update is called once per frame
     void Update() {
         // Crouch
         if (Input.GetKey(KeyCode.LeftControl))
         {
-            hitbox.size = hitbox.size * crouchedVector;
+            hitbox.size = new Vector2(boxcolliderWidth, boxcolliderHeigth * 0.5f);
             isCrouching = true;
         }
         else
         {
-            hitbox.size = new Vector2(1, 1);
+            hitbox.size = new Vector2(boxcolliderWidth, boxcolliderHeigth);
             isCrouching = false;
         }
-            
+        
+        
         // Slide
         if (isCrouching && Mv2.isGrounded && Input.GetKey(KeyCode.A) && !isSliding
             || isCrouching && Mv2.isGrounded && Input.GetKey(KeyCode.S) && !isSliding) 
@@ -47,10 +51,9 @@ public class Crouching : MonoBehaviour {
             isSliding = true;
             slideTimer = 0.0;
         }
-        if (isSliding && slideTimer < maxSlide)
+        if (isSliding && slideTimer < maxSlidingTime)
         {
             slideTimer += Time.deltaTime;
-            Mv2.moveSpeed = slideSpeed;
         }
         else
         {
