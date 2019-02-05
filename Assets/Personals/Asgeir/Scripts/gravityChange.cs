6595ee31962500ea8ec9b23cd43b623f4dd2de50
Krokou <asgeir@santi.no>
2019-02-05 19:02:29 +0100

    Made gravityChange script and test scene

--- Assets/Personals/Asgeir/Scripts/gravityChange.cs
+++ Assets/Personals/Asgeir/Scripts/gravityChange.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class gravityChange : MonoBehaviour {
+public class gravityChange : MonoBehaviour
+{
 
     public MovementV2 movement;
     public Transform TF_character;
     public Transform TF_camera;
 
+    public float duration = 1f;
+
     void flipGravity()
     {
         movement.ourGravity *= -1;
-        TF_camera.Rotate(new Vector3(0,0,180));
-        TF_character.Rotate(new Vector3(0,0,180));
+        TF_character.Rotate(new Vector3(0, 0, 180));
+
+        if (this.tag == "Player")
+        {
+            StartCoroutine(RotateCamera(duration));
+        }
     }
 
-	void Start () {
-		
-	}
-	
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
         if (movement.isGrounded) //Should
         {
             if (Input.GetKey(KeyCode.F))
@@ -27,5 +37,17 @@ public class gravityChange : MonoBehaviour {
                 flipGravity();
             }
         }
-	}
+    }
+
+    private IEnumerator RotateCamera(float duration)
+    {
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            TF_camera.transform.rotation = Quaternion.Slerp(TF_camera.transform.rotation, transform.rotation, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        TF_camera.transform.rotation = transform.rotation;
+    }
 }
