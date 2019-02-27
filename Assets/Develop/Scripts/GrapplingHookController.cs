3fe323c5003c5b73f11053ffde242337cd5a9915
Dabble <ddabble@users.noreply.github.com>
2019-02-27 01:35:47 +0100

    Added fireable grappling hook draft

---
+++ Assets/Develop/Scripts/GrapplingHookController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHookController : MonoBehaviour
+{
+	private const int PLAYER_LAYER = 8;
+	private const int PLAYER_WEAPONS_LAYER = 9;
+
+	public KeyCode fireKey = KeyCode.E;
+	public GameObject grapplingHook;
+
+	private bool hasFiredHook = false;
+
+	void Start()
+	{
+		Physics2D.IgnoreLayerCollision(PLAYER_LAYER, PLAYER_WEAPONS_LAYER);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(fireKey))
+			FireGrapplingHook();
+	}
+
+	private void FireGrapplingHook()
+	{
+		if (hasFiredHook)
+			return;
+
+		GrapplingHook firedGrapplingHook = Instantiate(grapplingHook, transform.position, Quaternion.identity, transform.parent)
+				.GetComponent<GrapplingHook>();
+		firedGrapplingHook.controller = this;
+		hasFiredHook = true;
+	}
+
+	public void OnGrapplingHookStopped()
+	{
+		hasFiredHook = false;
+	}
+}
