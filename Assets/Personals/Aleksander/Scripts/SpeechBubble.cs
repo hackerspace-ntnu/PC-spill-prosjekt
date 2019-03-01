3df308425e135133597b6e1fdb1b92998136d020
Aleksander Olsen <aleksaol@stud.ntnu.no>
2019-03-01 16:14:22 +0100

    Started on speech bubbles

---
+++ Assets/Personals/Aleksander/Scripts/SpeechBubble.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour {
+
+    GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+		parent = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
