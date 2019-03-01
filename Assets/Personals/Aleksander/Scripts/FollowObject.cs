24c68ab979026c4079082981f6870d9169249171
Aleksander Olsen <aleksaol@stud.ntnu.no>
2019-03-01 16:37:53 +0100

    Revert "Merge branch 'Feature/Familiar' of https://github.com/hackerspace-ntnu/PC-spill-prosjekt into Feature/Familiar"
    This reverts commit 9ce4954a2b1a307a55b7265aea01a78b3fda1323, reversing
    changes made to 3df308425e135133597b6e1fdb1b92998136d020.

--- Assets/Personals/Aleksander/Scripts/FollowObject.cs
+++ Assets/Personals/Aleksander/Scripts/FollowObject.cs
@@ -10,10 +10,7 @@ public class FollowObject : MonoBehaviour {
     public float distanceFactor;
     public float twitchFactor;
     public float twitchTolerance;
-<<<<<<< HEAD
     public float minVel;
-=======
->>>>>>> f6833535a6559c0cd2be92781b24e6a9b0ef54cb
 
     private Rigidbody2D thisRBody;
     private Rigidbody2D rBodyOfObj;
@@ -54,13 +51,8 @@ public class FollowObject : MonoBehaviour {
         // Set the target for the familiar in world coordinates.
         worldTargetPos = new Vector3(objToFollow.transform.position.x + targetPos.x, objToFollow.transform.position.y + targetPos.y, 0.0f);
 
-<<<<<<< HEAD
         // Set random twitch movment when familiar is close to player position.
         if (objToFollow.tag == "Player" && thisRBody.velocity.magnitude < twitchTolerance) {
-=======
-        // Set random twitch movment when familiar is close to target position.
-        if (thisRBody.velocity.magnitude < twitchTolerance) {
->>>>>>> f6833535a6559c0cd2be92781b24e6a9b0ef54cb
             worldTargetPos = new Vector3(worldTargetPos.x + Random.Range(-twitchFactor, twitchFactor), worldTargetPos.y + Random.Range(-twitchFactor, twitchFactor));
         }
     }
@@ -68,7 +60,6 @@ public class FollowObject : MonoBehaviour {
     private void FixedUpdate() {
         // Calculate distance to target position and set velocity towards target with speed increasing with distance.
         Vector2 distanceToTarget = transform.position - worldTargetPos;
-<<<<<<< HEAD
         Vector2 newVelocity = distanceToTarget.normalized * Mathf.Pow(distanceToTarget.magnitude, distanceFactor);
         thisRBody.velocity = -newVelocity;
 
@@ -81,11 +72,6 @@ public class FollowObject : MonoBehaviour {
             thisRBody.velocity = Vector2.zero;
             transform.position = worldTargetPos;
         }
-=======
-        distanceToTarget = distanceToTarget.normalized * Mathf.Pow(distanceToTarget.magnitude, distanceFactor);
-        
-        thisRBody.velocity = -distanceToTarget;
->>>>>>> f6833535a6559c0cd2be92781b24e6a9b0ef54cb
     }
 
     public void AttachToObject(GameObject obj, Vector2 target) {
