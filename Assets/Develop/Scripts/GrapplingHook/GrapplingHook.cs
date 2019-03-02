3a5558346a8605d4988a9daeac9e9066cd8e7dfe
Dabble <ddabble@users.noreply.github.com>
2019-03-02 21:08:03 +0100

    Code refactor

--- Assets/Develop/Scripts/GrapplingHook/GrapplingHook.cs
+++ Assets/Develop/Scripts/GrapplingHook/GrapplingHook.cs
@@ -14,9 +14,7 @@ public class GrapplingHook : MonoBehaviour
 
 	void Start()
 	{
-		direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - controller.transform.position;
-		direction.z = 0; // because it's not used
-		direction.Normalize();
+		direction = GetDirectionToVector(controller.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
 	}
 
 	void FixedUpdate()
@@ -24,10 +22,7 @@ public class GrapplingHook : MonoBehaviour
 		if (stopped)
 			return;
 
-		Vector3 nextPosition = transform.position;
-		nextPosition.x += direction.x * movementSpeed;
-		nextPosition.y += direction.y * movementSpeed;
-		transform.position = nextPosition;
+		transform.position = ExtendVectorInDirection(transform.position, direction, movementSpeed);
 
 		Vector3 firingDistance = transform.position - controller.transform.position;
 		if (firingDistance.magnitude >= maxFiringLength)
@@ -44,5 +39,28 @@ public class GrapplingHook : MonoBehaviour
 		stopped = true;
 		controller.OnGrapplingHookStopped();
 		GetComponent<BoxCollider2D>().enabled = false;
+	}
+
+	private static Vector3 GetDirectionToVector(Vector3 fromVector, Vector3 toVector)
+	{
+		Vector3 direction = toVector - fromVector;
+		direction.z = 0; // because it's not used
+		direction.Normalize();
+		return direction;
+	}
+
+
+	/// <summary>
+	///   <para>Returns a vector that has been extended a distance in the given direction from the base vector.</para>
+	/// </summary>
+	/// <param name="vec">The base vector.</param>
+	/// <param name="direction">The direction to extend in. Is assumed to be a normalized vector.</param>
+	/// <param name="distance"></param>
+	private static Vector3 ExtendVectorInDirection(Vector3 vec, Vector3 direction, float distance)
+	{
+		Vector3 extendedVec = vec;
+		extendedVec.x += direction.x * distance;
+		extendedVec.y += direction.y * distance;
+		return extendedVec;
 	}
 }
