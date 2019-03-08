f6903f4b566ed043285d8391351a951664c72973
Thor-Herman <thorherman.eggelen@gmail.com>
2019-03-08 13:30:09 +0100

    Bugfixes
    Changed from leftctrl to c, due to conflicts in editor. Changed back to movespeed.

--- Assets/Personals/T-H/Scripts/Crouching.cs
+++ Assets/Personals/T-H/Scripts/Crouching.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class Crouching : MonoBehaviour {
    
     // Crouch variables
-    private BoxCollider2D boxCollider;
-    private Movement Mv2;
+    public BoxCollider2D boxCollider;
+    private Movement Mv;
     private float boxcolliderHeight;
     private float boxcolliderWidth;
     private Vector2 boxcolliderOffset;
-    public Transform groundCollider;
     public GameObject[] triggers = new GameObject[2];
     public bool isCrouching;
-    public float crouchSpeed;
+    public float crouchSpeed = 3f;
 
     // Slide variables
+    private Rigidbody2D rb;
     private bool isSliding;
     public int slideSpeedModifier = 2;
     private float slideSpeed;
@@ -32,47 +32,40 @@ public class Crouching : MonoBehaviour {
     void Start () {
         isSliding = false;
         // Henter verdier til boxcollider
-        boxCollider = GetComponent<BoxCollider2D>();
         boxcolliderHeight = boxCollider.size.y;
         boxcolliderWidth = boxCollider.size.x;
 
         // Henter movementspeeds
-        Mv2 = GetComponent<Movement>();
-        moveSpeed = Mv2.moveSpeed;
+        Mv = GetComponent<Movement>();
+        moveSpeed = Mv.moveSpeed;
         slideSpeed = moveSpeed + slideSpeedModifier;
+        // Burde egentlig hente movespeed
+        rb = GetComponent<Rigidbody2D>();
         timeSpentNotSliding = 2f;
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(timeSpentNotSliding);
 
         // Crouch
-        if (Input.GetKey(KeyCode.LeftControl) && Mv2.isGrounded && !isCrouching) // Må vel egentlig bruke GetKeyDown
-        {
+        if (Input.GetKeyDown(KeyCode.C) && !isCrouching)
+        { 
             Crouch();
         }
-        else if ((Input.GetKey(KeyCode.LeftControl))) 
-        {
-            //Kode her?
-            Debug.Log("Now crouching");
-        }
-        else // if (Input.GetKeyUp(KeyCode.LeftControl))
+        else if (Input.GetKeyUp(KeyCode.C))
         {
             StopCrouch();
             Debug.Log("Stopped crouching");
         }
 
-
-
         // Slide
+
         calculateTimeSpentMoving(); // Sjekker hvor lenge man har beveget seg før man prøver å slide
 
-        if (CheckStartSlide() && timeSpentNotSliding >= slideCooldown)
+        if (CheckStartSlide())
         {
             isSliding = true;
             slideTimer = 0;
-            Mv2.moveSpeed = slideSpeed;
         }
 
         if (isSliding && slideTimer < maxSlidingTime)
@@ -103,7 +96,7 @@ public class Crouching : MonoBehaviour {
 
     private void calculateTimeSpentMoving()
     {
-        if (Mathf.Abs(Mv2.moveHorizontal) == 1)
+        if (Mathf.Abs(Mv.moveHorizontal) == 1)
         {
             timeSpentMoving += Time.deltaTime;
         }
@@ -117,28 +110,29 @@ public class Crouching : MonoBehaviour {
     {
         boxCollider.offset = new Vector2(0, boxcolliderHeight * 0.125f);
         boxCollider.size = new Vector2(boxcolliderWidth, boxcolliderHeight * 0.75f);
-        groundCollider.localPosition = new Vector2(0, -boxcolliderHeight / 2 + boxcolliderHeight * 0.25f);
         isCrouching = true;
         ActivateTriggers(triggers, false);
         if (!isSliding)
         {
-            Mv2.moveSpeed = crouchSpeed;
+            Mv.moveSpeed = crouchSpeed;
+            //rb.velocity = new Vector2(crouchSpeed, rb.velocity.y);
         }
     }
     private void StopCrouch()
     {
         boxCollider.size = new Vector2(boxcolliderWidth, boxcolliderHeight);
         boxCollider.offset = new Vector2(0, 0);
-        groundCollider.localPosition = new Vector2(0, -boxcolliderHeight / 2);
         isCrouching = false;
         ActivateTriggers(triggers, true);
-        Mv2.moveSpeed = moveSpeed;
+        Mv.moveSpeed = moveSpeed;
+        //rb.velocity = new Vector2(moveSpeed, rb.velocity.y); // Usikker på dette
     }
 
     
     private bool CheckStartSlide()
     { 
-        if (isCrouching && timeSpentMoving > timeSpentMovingMinimum && !isSliding && Mv2.isGrounded)
+        if (isCrouching && timeSpentMoving >= timeSpentMovingMinimum && !isSliding && Mv.isGrounded && 
+            timeSpentNotSliding >= slideCooldown)
         {
             return true;
         }
