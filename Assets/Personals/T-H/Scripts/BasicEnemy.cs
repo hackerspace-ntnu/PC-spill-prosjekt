fcdd142d4fa4664212b8b132854760b50fdfdcd0
Thor-Herman <thorherman.eggelen@gmail.com>
2019-04-09 20:05:32 +0200

    Branch creation

---
+++ Assets/Personals/T-H/Scripts/BasicEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicEnemy : MonoBehaviour
+{
+    // Health
+    public int startingHealth;
+    private int currentHealth;
+
+    // Navigation
+    NavMeshAgent navMeshAgent;
+
+
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+}
