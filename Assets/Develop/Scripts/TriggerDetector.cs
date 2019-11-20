458e9d746a40bc4e01b84cda9078b36fc5f8cc12
Aleksander Olsen <aleksaol@stud.ntnu.no>
2019-11-20 21:46:50 +0100

    Fixed crouch that Sondre broke (after merge conflicts)

--- Assets/Develop/Scripts/TriggerDetector.cs
+++ Assets/Develop/Scripts/TriggerDetector.cs
@@ -7,6 +7,8 @@ public class TriggerDetector : MonoBehaviour
     [SerializeField]
     private PlayerController controller;
 
+    private int CeilingCount = 0;   // Keep track of number of objects above player when crouching.
+
     // Use this for initialization
     void Start()
     {
@@ -17,12 +19,16 @@ public class TriggerDetector : MonoBehaviour
     {
         if (collision.GetComponent<Collider2D>().tag == "Standard")
         {
+
+            if (this.gameObject.tag == "CeilingDetector") {
+                CeilingCount++;
+                controller.CanUncrouch = false;
+            }
+
             if (this.gameObject.name == "Ground Trigger")
             {
                 controller.Grounded = true;
             }
-
-
             else if (this.gameObject.name == "Wall Trigger Left")
             {
                 controller.WallTrigger = 1;
@@ -31,12 +37,20 @@ public class TriggerDetector : MonoBehaviour
             {
                 controller.WallTrigger = -1;
             }
-            print("ENTER" + this.gameObject.name);
         }
     }
 
-    private void OnTriggerExit2D(Collider2D col)
+    private void OnTriggerExit2D(Collider2D collision)
     {
+
+        if (collision.GetComponent<Collider2D>().tag == "Standard" && this.gameObject.tag == "CeilingDetector") {
+            CeilingCount--;
+
+            if (CeilingCount <= 0) {
+                controller.CanUncrouch = true;
+            }
+        }
+
         if (this.gameObject.name == "Ground Trigger")
         {
             controller.Grounded = false;
@@ -45,6 +59,5 @@ public class TriggerDetector : MonoBehaviour
         {
             controller.WallTrigger = 0; //Must set this globally, not just for the current state
         }
-        print("EXIT " + this.gameObject.name);
     }
 }
