7163a2a7ae08ba64e4e4def42da6fa741f64cfe7
Dabble <ddabble@users.noreply.github.com>
2019-11-21 13:42:07 +0100

    Whitespace cleanup

--- Assets/Develop/Scripts/GrapplingHook/GrapplingHook.cs
+++ Assets/Develop/Scripts/GrapplingHook/GrapplingHook.cs
@@ -5,66 +5,66 @@ using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
 {
-	public GrapplingHookController controller;
-	public float movementSpeed = 0.2f;
-	public float maxFiringLength = 4.0f;
+    public GrapplingHookController controller;
+    public float movementSpeed = 0.2f;
+    public float maxFiringLength = 4.0f;
 
-	private Vector3 direction;
-	private bool stopped = false;
+    private Vector3 direction;
+    private bool stopped = false;
 
-	void Start()
-	{
-		direction = GetDirectionToVector(controller.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-	}
+    void Start()
+    {
+        direction = GetDirectionToVector(controller.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
 
-	void FixedUpdate()
-	{
-		if (stopped)
-			return;
+    void FixedUpdate()
+    {
+        if (stopped)
+            return;
 
-		transform.position = ExtendVectorInDirection(transform.position, direction, movementSpeed);
+        transform.position = ExtendVectorInDirection(transform.position, direction, movementSpeed);
 
-		Vector3 firingDistance = transform.position - controller.transform.position;
-		if (firingDistance.magnitude >= maxFiringLength)
-		{
-			Stop();
-			Vector3 directionFromController = GetDirectionToVector(controller.transform.position, transform.position);
-			transform.position = ExtendVectorInDirection(controller.transform.position, directionFromController, maxFiringLength);
-		}
-	}
+        Vector3 firingDistance = transform.position - controller.transform.position;
+        if (firingDistance.magnitude >= maxFiringLength)
+        {
+            Stop();
+            Vector3 directionFromController = GetDirectionToVector(controller.transform.position, transform.position);
+            transform.position = ExtendVectorInDirection(controller.transform.position, directionFromController, maxFiringLength);
+        }
+    }
 
-	void OnTriggerEnter2D(Collider2D collider)
-	{
-		Stop();
-	}
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Stop();
+    }
 
-	private void Stop()
-	{
-		stopped = true;
-		controller.OnGrapplingHookStopped();
-		GetComponent<BoxCollider2D>().enabled = false;
-	}
+    private void Stop()
+    {
+        stopped = true;
+        controller.OnGrapplingHookStopped();
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
 
-	private static Vector3 GetDirectionToVector(Vector3 fromVector, Vector3 toVector)
-	{
-		Vector3 direction = toVector - fromVector;
-		direction.z = 0; // because it's not used
-		direction.Normalize();
-		return direction;
-	}
+    private static Vector3 GetDirectionToVector(Vector3 fromVector, Vector3 toVector)
+    {
+        Vector3 direction = toVector - fromVector;
+        direction.z = 0; // because it's not used
+        direction.Normalize();
+        return direction;
+    }
 
 
-	/// <summary>
-	///   <para>Returns a vector that has been extended a distance in the given direction from the base vector.</para>
-	/// </summary>
-	/// <param name="vec">The base vector.</param>
-	/// <param name="direction">The direction to extend in. Is assumed to be a normalized vector.</param>
-	/// <param name="distance"></param>
-	private static Vector3 ExtendVectorInDirection(Vector3 vec, Vector3 direction, float distance)
-	{
-		Vector3 extendedVec = vec;
-		extendedVec.x += direction.x * distance;
-		extendedVec.y += direction.y * distance;
-		return extendedVec;
-	}
+    /// <summary>
+    ///   <para>Returns a vector that has been extended a distance in the given direction from the base vector.</para>
+    /// </summary>
+    /// <param name="vec">The base vector.</param>
+    /// <param name="direction">The direction to extend in. Is assumed to be a normalized vector.</param>
+    /// <param name="distance"></param>
+    private static Vector3 ExtendVectorInDirection(Vector3 vec, Vector3 direction, float distance)
+    {
+        Vector3 extendedVec = vec;
+        extendedVec.x += direction.x * distance;
+        extendedVec.y += direction.y * distance;
+        return extendedVec;
+    }
 }
