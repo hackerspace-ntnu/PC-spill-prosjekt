7b4ee9c696f5af02b3038eda586d99f55cf427fa
Joachmak <joachmak@users.noreply.github.com>
2020-01-14 11:10:44 +0100

    Added secret room fade in and out on enter.

--- Assets/Develop/Scripts/SecretRooms/SecretRoom.cs
+++ Assets/Develop/Scripts/SecretRooms/SecretRoom.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class SecretRoom : MonoBehaviour
 {
+    private byte time = 255;
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Enter");
-        this.GetComponent<SpriteRenderer>().enabled = false;
+        StopAllCoroutines();
+        StartCoroutine(fadeOut());
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         Debug.Log("Exit");
-        this.GetComponent<SpriteRenderer>().enabled = true;
+        StopAllCoroutines();
+        StartCoroutine(fadeIn());
+    }
+
+    IEnumerator fadeOut()
+    {
+        SpriteRenderer renderer = this.GetComponent<SpriteRenderer>();
+        Color32 color = renderer.color; // get color 
+        while (time > 0) // fade
+        {
+            time -= 15;
+            color.a = time;
+            renderer.color = color;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    IEnumerator fadeIn()
+    {
+        SpriteRenderer renderer = this.GetComponent<SpriteRenderer>();
+        Color32 color = renderer.color; // get color 
+        while (time < 255) // fade
+        {
+            time += 15;
+            color.a = time;
+            renderer.color = color;
+            yield return new WaitForSeconds(0.01f);
+        }
+        
     }
 }
