ec8a7a3ad5bfeb58626e8f10bf88bb494e7a0676
Joachmak <joachmak@users.noreply.github.com>
2020-01-21 11:12:23 +0100

    Fixed lag glitch. Enemy continues in the same direction as when lag glitch is initiated.

--- Assets/Develop/Scripts/LagGlitch/Scripts/EnemyMovement.cs
+++ Assets/Develop/Scripts/LagGlitch/Scripts/EnemyMovement.cs
@@ -4,30 +4,56 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+
     GameObject clone;
-    public void boxColliderToggle(bool turnOn)
+    Color32 ghostColor = new Color32(100, 100, 100, 200);
+    Color32 regularColor;
+    bool lagActive = false;
+
+    public void Start()
     {
-        this.GetComponent<BoxCollider2D>().enabled = turnOn;
-        if (turnOn)
-        {
-            //movementDir /= 2; // turn on movement
-            this.GetComponent<SpriteRenderer>().enabled = true; // turn on sprite renderer
-            Destroy(clone);
-            Debug.Log("1");
-        }
-        else
-        {
-            //movementDir *= 2; // turn off movement
-            clone = Instantiate(this.gameObject);
-            this.GetComponent<SpriteRenderer>().enabled = false; // turn off sprite renderer
-            clone.GetComponent<EnemyMovement>().moving = false;
-            Debug.Log("2");
-        }
+        regularColor = this.GetComponent<SpriteRenderer>().color;
     }
+
+
+
+
+    public void manageLagMovement(bool on) // i have no access to the current movement script, so i make one here
+    {
+        // when lag movement turns on, enemy keeps going in its given direction until lag movement is turned off. 
+        // When lag movement turns off, enemy returns to its regular movement pattern.
+
+        lagActive = on; // this variable should make direct changes to the movement pattern of enemies
+    }
+    
 
     int movementDir = 2; // 2 = left, 3 = right
     Vector3 movement = new Vector3(0.05f, 0, 0);
     public bool moving = true;
+    
+    public void toggleLagGlitch(bool on)
+    {
+        this.GetComponent<BoxCollider2D>().enabled = !on;
+        Debug.Log(this.name);
+        if (on)
+        {
+            clone = Instantiate(this.gameObject); // clone enemy 
+            clone.GetComponent<EnemyMovement>().moving = false; // make clone stand still
+            this.GetComponent<SpriteRenderer>().color = ghostColor; // give original enemy ghost-color
+            moving = false; // regular movement turns off 
+            manageLagMovement(true); // turn on lag glitch - movement
+        }
+        else
+        {
+            Destroy(clone);
+            this.GetComponent<SpriteRenderer>().color = regularColor;
+            manageLagMovement(false); // stop lag glitch - movement
+            moving = true; // return to original movement 
+        }
+    }
+
+
+
     public void Update()
     {
         if(moving)
@@ -40,6 +66,16 @@ public class EnemyMovement : MonoBehaviour
             {
                 movementDir = 2;
             }
+            if (movementDir == 2)
+            {
+                this.transform.position -= movement;
+            }
+            else if (movementDir == 3)
+            {
+                this.transform.position += movement;
+            }
+        } else if (lagActive) // don't switch direction, only move
+        {
             if (movementDir == 2)
             {
                 this.transform.position -= movement;
