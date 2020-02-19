7096f09bad89523ab0320c690efef4d686343557
Asgeir Santi <asgeir@santi.no>
2020-02-19 12:22:23 +0100

    Boss Jump
    This is part of the lick script.

---
+++ Assets/BossGroundDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGroundDetection : MonoBehaviour
+{
+    BossLick bossLick;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossLick = GetComponentInParent<BossLick>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            bossLick.StartCoroutine("Jump");
+        }
+    }
+
+
+}
