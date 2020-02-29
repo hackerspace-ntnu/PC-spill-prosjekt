63d3488acfffd513eaa43f0fd1d4eb9ad5850369
Joachmak <joachmak@users.noreply.github.com>
2020-02-29 17:40:52 +0100

    Added parallax and updated final level-scene (under testSceneSprite).

---
+++ Assets/Develop/Scripts/Visual/CameraColliderIgnore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraColliderIgnore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        BoxCollider2D collider = this.GetComponent<BoxCollider2D>();
+        Physics.IgnoreLayerCollision(12, 0, true);
+    }
+}
