66b1166d2804baabf86592a892d400fe6687b864
Joachmak <joachmak@users.noreply.github.com>
2020-03-04 14:13:46 +0100

    Expanded level, added more trees and decorations, added climbable vines, made other walls (walls without vines) impossible to climb on.

--- Assets/ParallaxComponent.cs
+++ Assets/ParallaxComponent.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class ParallaxComponent : MonoBehaviour
 {
     public float distance;
-    private float x;
     private Transform camTransform;
     private float prevX;
+    private float prevY;
     // Start is called before the first frame update
     void Start()
     {
         camTransform = Camera.main.transform;
-        x = this.transform.position.x - Camera.main.transform.position.x;
         prevX = camTransform.position.x;
+        prevY = camTransform.position.y;
     }
 
     // Update is called once per frame
@@ -22,11 +22,14 @@ public class ParallaxComponent : MonoBehaviour
     void LateUpdate()
     {
         float newX = camTransform.position.x;
-        if(newX!=prevX)
+        float newY = camTransform.position.y;
+        if(newX!=prevX || newY!=prevY)
         {
-            float offset = (newX-prevX) * distance;
-            this.transform.position = new Vector3(Mathf.Lerp(this.transform.position.x,this.transform.position.x + offset,0.15f), this.transform.position.y, this.transform.position.z);
+            float offsetX = (newX-prevX) * distance;
+            float offsetY = (newY - prevY) * distance;
+            this.transform.position = new Vector3(Mathf.Lerp(this.transform.position.x,this.transform.position.x + offsetX,0.15f), Mathf.Lerp(this.transform.position.y, this.transform.position.y + offsetY, 0.15f), this.transform.position.z);
         }
         prevX = newX;
+        prevY = newY;
     }
 }
